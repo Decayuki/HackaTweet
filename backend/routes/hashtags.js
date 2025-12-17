083142ac02cc1612aa7061f15975e0d3fb343ee6
Your name <Your email>
2025-12-17 19:50:54 +0100

    updated route hashtag

--- backend/routes/hashtags.js
+++ backend/routes/hashtags.js
@@ -13,21 +13,5 @@ router.get("/", async (req, res) => {
 
   res.json({ result: true, hashtags });
 });
-// Get récupérer tous les tweets selon hashtag
-router.get("/:tag", (req, res) => {
-  const tag = req.params.tag.toLocaleLowerCase();
-  Hashtag.findOne({ tag })
-    .populate({
-      // Remplace les ObjectId des tweets par les documents tweets complets.
-      path: "tweets",
-      // Pour chaque tweet : remplace l'ObjetId user, par l'objet user, en ne gardant que username et firstname
-      populate: { path: "user", select: "username firsname" },
-    })
-    .then((hashtag) => {
-      if (!hashtag) {
-        res.json({ result: false, message: "Hashtag not found" });
-      }
-    });
-});
 
 module.exports = router;
