d1a70bae45cde1aa7da94a404d06bde11f6b308b
Decayuki <elmen.marc@gmail.com>
2025-12-17 19:56:36 +0100

    Merge & Update : Routes ok

--- backend/routes/hashtags.js
+++ backend/routes/hashtags.js
@@ -13,4 +13,24 @@ router.get('/', async (req, res) => {
   res.json({ result: true, hashtags });
 });
 
+router.get("/:tag", (req, res) => {
+  // L'URL sera /hashtags/react, on ajoute le # pour chercher "#react" en DB
+  const tag = `#${req.params.tag.toLowerCase()}`;
+
+  Hashtag.findOne({ tag }) //({ tag: '#' + tag }) ou... #${tag}
+    .populate({
+      path: "tweets",
+      populate: { path: "user", select: "username firstname" },  
+    })
+    .then((hashtag) => {
+      if (!hashtag) {
+        return res.json({ result: false, error: "Hashtag not found" });
+      }
+      res.json({ result: true, hashtag });
+    })
+    .catch((error) => {
+      res.json({ result: false, error: "Failed to fetch hashtag" });
+    });
+});
+
 module.exports = router;
