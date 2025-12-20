972afd6319921a45f4a02650e25f9157f3615179
Decayuki <elmen.marc@gmail.com>
2025-12-20 12:27:21 +0100

    go

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,32 +1,11 @@
 import { Provider } from 'react-redux';
-import { store } from '../Redux/store'; 
-import { configureStore } from "@reduxjs/toolkit";
-import user from "../reducers/user";
-import { persistStore, persistReducer } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import storage from "redux-persist/lib/storage";
-
-// Import du store
-// Provider permet de partager le store à tous les composants
-//
-
-
-const persistConfig = { key: "HackaTwwet", storage };
-
-const store = configureStore({
-  reducer: persistReducer(persistConfig, reducers),
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
-});
-
-const persistor = persistStore(store);
+import { store } from '../Redux/store'; // ← Store unique configuré ici
+import '../styles/globals.css'; // ← À adapter si tu as un fichier global CSS
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
       <Component {...pageProps} />
-       </PersistGate>
     </Provider>
   );
 }
